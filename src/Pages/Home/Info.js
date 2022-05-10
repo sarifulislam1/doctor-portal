7a82235209a250022368bb22ed136ebeb65fdb84
Your Name <sarifulislaml8280@gmail.com>
2022-05-10 19:20:59 +0600

    info ready

--- src/Pages/Home/Info.js
+++ src/Pages/Home/Info.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import clock from '../../assets/icons/clock.svg'
+import marker from '../../assets/icons/marker.svg'
+import phone from '../../assets/icons/phone.svg'
 
 const Info = () => {
     return (
-        <div>
-
+        <div className='grid grid-cols-1 lg:grid-cols-3 gap-5 mx-8 mb-8'>
+            <div class="card lg:card-side bg-base-100 shadow-xl bg-gradient-to-r from-secondary to-primary">
+                <figure><img className='mx-8 my-8' src={clock} /></figure>
+                <div class="card-body text-white">
+                    <h2 class="card-title text-white">Opening Hours</h2>
+                    <p>Click the button to listen on Spotiwhy app.</p>
+                </div>
+            </div>
+            <div class="card lg:card-side bg-base-100 shadow-xl bg-accent">
+                <figure><img className='mx-8 my-8' src={marker} /></figure>
+                <div class="card-body text-white">
+                    <h2 class="card-title">Visit our location</h2>
+                    <p>Click the button to listen on Spotiwhy app.</p>
+                </div>
+            </div>
+            <div class="card lg:card-side bg-base-100 shadow-xl bg-gradient-to-r from-secondary to-primary">
+                <figure><img className='mx-8 my-8' src={phone} /></figure>
+                <div class="card-body text-white">
+                    <h2 class="card-title">Contact us now</h2>
+                    <p>+000 123 456789</p>
+                </div>
+            </div>
         </div>
     );
 };
